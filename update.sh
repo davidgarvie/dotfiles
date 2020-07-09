46512239bbde848d2c3cead7407f1d0da1741c20
David Garvie <garvie88@gmail.com>
2020-07-09 16:56:52 +0100

    updating dotfiles on Thu  9 Jul 2020 15:56:52 UTC

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-BREW_INSTALLS_FILE="$REPO_DIR/brew_bundle_dump"
-REPO_DIR="$HOME/projects/dotfiles"
-VSCODE_EXTENSIONS_FILE="$REPO_DIR/vscode/extensions"
-
-cat $VSCODE_EXTENSIONS_FILE | xargs /usr/local/bin/code --install-extension
-
-brew bundle install --file="$BREW_INSTALLS_FILE"
